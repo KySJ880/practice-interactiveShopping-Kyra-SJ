e3a247e3cb7557e3deeef4ce1bf40b1afe4449b8
KySJ <smithkyra133@gmail.com>
2025-06-10 20:49:07 -0500

    Shopping Cart

--- Script1.js
+++ Script1.js
@@ -1,2 +1,8 @@
 const cartItem = document.getElementById("cart-item");
-const addButton = document.getElementById("add-button")
+const addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addCartItem);
+function addCartItem () {
+    const newItem = cartItem.value;
+    const listItem = document.createElement("li");
+listItem.textContent = newItem;
+}
